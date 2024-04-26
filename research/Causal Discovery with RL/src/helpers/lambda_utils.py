fix: Fix ls / lu computation for score normalization

research/Causal Discovery with RL/src/helpers/lambda_utils.py
@@ -4,11 +4,71 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from scipy.spatial.distance import pdist, squareform
 
+from rewards.Reward_BIC import PyTorchMLP2
+import torch
+from torch import nn
+from torch import optim
+
+def fit_and_err(X_train, y_train):  # Working for linear case : calculate_LR ()
+    device = 'mps'
+
+    input_size = X_train.shape[1]
+    model = PyTorchMLP2(input_size=input_size, hidden_size=64, output_size=1).to(device)
+
+    # Convert to pytorch
+    X_train = torch.from_numpy(X_train.astype(np.float32)).to(device)
+    y_train = torch.from_numpy(y_train.astype(np.float32)).to(device)
+    y_train = y_train.unsqueeze(1)  # going ftom 1-D to 2-D
+    # print("X_train", X_train)
+
+    # print (y_train)
+
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)  # , weight_decay=1e-5)
+
+    # Training loop
+    # model.train()
+    num_epochs = 100
+    for epoch in range(num_epochs):
+        # print("ccc")
+
+        outputs = model(X_train)
+        loss = criterion(outputs, y_train)  # _torch.view(-1, 1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Print progress
+        if (epoch + 1) % 100 == 0:
+            # print(outputs)
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+    # print("qr")
+
+    # Making predictions (inference)
+    model.eval()
+    with torch.no_grad():
+        predictions = model(X_train)
+
+    # Convert back to numpy
+    y_pred = predictions.cpu().numpy()
+
+    # Compute the error
+    # y_err = y_pred.flatten() - y_train
+    y_train = y_train.cpu().numpy()
+    y_err = y_pred - y_train
+
+    # print(y_err)
+
+    return y_err
+
 
 def BIC_input_graph(X, g, reg_type='LR', score_type='BIC'):
     """cal BIC score for given graph"""
 
     RSS_ls = []
+    RSS_ls_2 = []
 
     n, d = X.shape 
 
@@ -23,6 +83,7 @@ def BIC_input_graph(X, g, reg_type='LR', score_type='BIC'):
         y_ = X[:, [i]]   #it separates the feature corresponding to the current index i from the rest of the data.
         inds_x = list(np.abs(g[i])>0.1)
 
+        mlp_success = False
         if np.sum(inds_x) < 0.1:   
             y_pred = np.mean(y_)
         else:
@@ -34,17 +95,24 @@ def BIC_input_graph(X, g, reg_type='LR', score_type='BIC'):
                 X_ = X_ / med_w
             reg.fit(X_, y_)
             y_pred = reg.predict(X_)
+            mlp_success = True
+            err_mlp = fit_and_err(X_, y_)
         RSSi = np.sum(np.square(y_ - y_pred))
+        if mlp_success:
+            RSSi_2 = np.sum(err_mlp)
+        else:
+            RSSi_2 = RSSi
 
         if reg_type == 'GPR':
             RSS_ls.append(RSSi+1.0)
         else:
             RSS_ls.append(RSSi)
+        RSS_ls_2.append(RSSi_2)
 
     if score_type == 'BIC':
-        return np.log(np.sum(RSS_ls)/n+1e-8) 
+        return np.log(np.sum(RSS_ls)/n+1e-8), np.log(np.sum(RSS_ls_2)/n+1e-8)
     elif score_type == 'BIC_different_var':
-        return np.sum(np.log(np.array(RSS_ls)/n)+1e-8) 
+        return np.sum(np.log(np.array(RSS_ls)/n)+1e-8), np.sum(np.log(np.array(RSS_ls_2)/n)+1e-8)
     
     
 def BIC_lambdas(X, gl=None, gu=None, gtrue=None, reg_type='LR', score_type='BIC'):
@@ -76,17 +144,24 @@ def BIC_lambdas(X, gl=None, gu=None, gtrue=None, reg_type='LR', score_type='BIC'
     if gu is None:
         gu = np.zeros((d, d))
 
-    sl = BIC_input_graph(X, gl, reg_type, score_type) # Bic score for lower bound graph
-    su = BIC_input_graph(X, gu, reg_type, score_type) # Bic score for upper bound graph
+    sl, sl_mlp = BIC_input_graph(X, gl, reg_type, score_type) # Bic score for lower bound graph
+    su, su_mlp = BIC_input_graph(X, gu, reg_type, score_type) # Bic score for upper bound graph
 
     if gtrue is None:
         strue = sl - 10
+        strue_mlp = sl_mlp - 10
     else:
         print(BIC_input_graph(X, gtrue, reg_type, score_type))  #print initial BIC score for true graph
         print(gtrue)
         print(bic_penalty)
-        strue = BIC_input_graph(X, gtrue, reg_type, score_type) + np.sum(gtrue) * bic_penalty  #final score for true graph with penalty
+        strue, strue_mlp = BIC_input_graph(X, gtrue, reg_type, score_type) + np.sum(gtrue) * bic_penalty  #final score for true graph with penalty
         #notice that penalty discourages the use of more variables in the model (more edges in the graph)
         #so the score is penalized with the number of edges in the graph
-    return sl, su, strue  #return lower bound, upper bound and true score
+
+    # print("slX", sl, sl_mlp)
+    # print("suX", su, su_mlp)
+    # print("strueX", strue, strue_mlp)
+    # 0/0
+    # return sl, su, strue  #return lower bound, upper bound and true score
+    return sl_mlp, su_mlp, strue_mlp  # TODO: This is new one, with MLP. | return lower bound, upper bound and true score
 

research/Causal Discovery with RL/src/main.py
@@ -18,6 +18,7 @@ from pytz import timezone
 from datetime import datetime
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from helpers.cam_with_pruning_cam import pruning_cam
 
 from data_loader import DataGenerator_read_data
 from models import Actor
@@ -107,7 +108,12 @@ def main():
     if config.lambda_flag_default:
         
         sl, su, strue = BIC_lambdas(training_set.inputdata, None, None, training_set.true_graph.T, reg_type, score_type)
-        
+        # print(sl, su, strue)
+        # 0/0
+        # sl = 1.2705835383149398
+        # su = 5.729847963164211
+        # strue = 2.4855898888731427
+
         lambda1 = 0
         lambda1_upper = 5
         lambda1_update_add = 1
@@ -324,6 +330,11 @@ def main():
             plt.savefig('{}/recovered_graph_iteration_{}.png'.format(config.plot_dir, image_count))
             plt.close()
 
+        # ls_kv = callreward.update_all_scores(lambda1, lambda2)
+        # graph_int, score_min, cyc_min = np.int32(ls_kv[0][0]), ls_kv[0][1][1], ls_kv[0][1][-1]
+        # graph_batch = convert_graph_int_to_adj_mat(graph_int)
+        # graph_batch_pruned = np.transpose(pruning_cam(training_set.inputdata, np.array(graph_batch).T))
+
         # update lambda1, lamda2
         if (i+1) % lambda_iter_num == 0:
             ls_kv = callreward.update_all_scores(lambda1, lambda2)
@@ -356,7 +367,7 @@ def main():
             elif reg_type == 'QR':
                 graph_batch_pruned = np.array(graph_prunned_by_coef_2nd(graph_batch, training_set.inputdata))
             elif reg_type == 'GPR':
-                from helpers.cam_with_pruning_cam import pruning_cam
+                # from helpers.cam_with_pruning_cam import pruning_cam
                 #raise NotImplementedError('Check proper inputdatastallation of R')
                 # The R codes of CAM pruning operates the graph form that (i,j)=1 indicates i-th node-> j-th node
                 # so we need to do a tranpose on the input graph and another tranpose on the output graph

research/Causal Discovery with RL/src/rewards/Reward_BIC.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 import logging
 import tensorflow as tf
+from helpers.analyze_utils import cache_write
 
 from helpers.debugger import print_mine, print_mine_np
 import torch
@@ -32,7 +33,7 @@ class PyTorchMLP2(nn.Module):
         self.fcl = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = torch.nn.functional.leaky_relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc1(x))
         # x = torch.nn.functional.leaky_relu(self.fc2(x))
         # x = torch.nn.functional.leaky_relu(self.fc3(x))
 
@@ -267,19 +268,23 @@ class get_Reward(object):
     '''
 
     def calculate_LR(self, X_train, y_train):
+        device = 'mps'
+
         input_size = X_train.shape[1]
-        model = PyTorchMLP1(input_size=input_size)
+        model = PyTorchMLP1(input_size=input_size).to(device)
 
         # Convert X_train, y_train from NumPy arrays to PyTorch tensors
-        X_train_torch = torch.from_numpy(X_train.astype(np.float32))
-        y_train_torch = torch.from_numpy(y_train.astype(np.float32))
+        X_train_torch = torch.from_numpy(X_train.astype(np.float32)).to(device)
+        y_train_torch = torch.from_numpy(y_train.astype(np.float32)).to(device)
 
         criterion = nn.MSELoss()
         optimizer = optim.Adam(model.parameters(), lr=0.01)
 
         # Training loop
         model.train()
-        for epoch in range(100):  # Assuming a fixed number of epochs
+        # t1 = time.time()
+        num_epochs = 100
+        for epoch in range(num_epochs):  # Assuming a fixed number of epochs
             # print("ccc")
             optimizer.zero_grad()
             outputs = model(X_train_torch)
@@ -287,13 +292,20 @@ class get_Reward(object):
             loss.backward()
             optimizer.step()
 
+            if (epoch + 1) % 100 == 0:
+                # print(outputs)
+                print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+        # print("Time taken", time.time() - t1, "seconds")
+
         # Making predictions (inference)
         model.eval()
         with torch.no_grad():
             predictions = model(X_train_torch)
 
         # Convert predictions back to a NumPy array
-        y_pred = predictions.numpy()
+        y_pred = predictions.to('cpu').numpy()
+
+        # print(y_pred.shape, y_train.shape)
 
         # Compute the error
         y_err = y_pred.flatten() - y_train  # Ensure shapes are compatible
@@ -342,12 +354,14 @@ class get_Reward(object):
 
 
     def calculate_GPR(self, X_train, y_train): #Working for linear case : calculate_LR ()
+        device = 'mps'
+
         input_size = X_train.shape[1]
-        model = PyTorchMLP2(input_size=input_size, hidden_size=100, output_size=1)
+        model = PyTorchMLP2(input_size=input_size, hidden_size=64, output_size=1).to(device)
 
         # Convert to pytorch
-        X_train = torch.from_numpy(X_train.astype(np.float32))
-        y_train = torch.from_numpy(y_train.astype(np.float32))
+        X_train = torch.from_numpy(X_train.astype(np.float32)).to(device)
+        y_train = torch.from_numpy(y_train.astype(np.float32)).to(device)
         y_train = y_train.unsqueeze(1)  #going ftom 1-D to 2-D
         #print("X_train", X_train)
 
@@ -358,7 +372,7 @@ class get_Reward(object):
 
         # Training loop
         #model.train()
-        num_epochs = 1000
+        num_epochs = 100
         for epoch in range(num_epochs):
             #print("ccc")
 
@@ -372,7 +386,8 @@ class get_Reward(object):
             # Print progress
             if (epoch + 1) % 100 == 0:
                 # print(outputs)
-                print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+                if np.random.uniform() < 0.05:
+                    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
 
         #print("qr")
 
@@ -382,12 +397,14 @@ class get_Reward(object):
             predictions = model(X_train)
 
         # Convert back to numpy
-        y_pred = predictions.numpy()
+        y_pred = predictions.cpu().numpy()
 
         # Compute the error
         #y_err = y_pred.flatten() - y_train
-        y_train = y_train.numpy()
+        y_train = y_train.cpu().numpy()
         y_err = y_pred - y_train
+
+        # print(y_err)
 
         return y_err
 
@@ -529,13 +546,14 @@ class get_Reward(object):
                 y_err = self.calculate_yerr(X_train, y_train)
 
 
-
+            # print(y_err)
             RSSi = np.sum(np.square(y_err))
+            # cache_write("y_err", y_err)
 
             # if the regresors include the true parents, GPR would result in very samll values, e.g., 10^-13
             # so we add 1.0, which does not affect the monotoniticy of the score
-            if self.reg_type == 'GPR':
-                RSSi += 1.0
+            # if self.reg_type == 'GPR': TODO: CHANGE IT BACK
+            #     RSSi += 1.0
 
             RSS_ls.append(RSSi)
             self.d_RSS[graph_to_int[i]] = RSSi